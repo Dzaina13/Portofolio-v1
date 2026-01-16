0d55c5d58b7e0b625796a2d891a61ce68156e4c5
Mochammad Dzaky - XII RPL C <int-dzaky@lalampa.me>
2026-01-16 20:53:28 +0700

    main layout fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { AnimatedWaveComponent } from './animated-wave/animated-wave.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { SectionComponent } from './section/section.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AnimatedWaveComponent, NavbarComponent,SectionComponent],
+  imports: [AnimatedWaveComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
